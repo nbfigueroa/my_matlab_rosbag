ed10e8f5ff3a86c35c5529c95f059abcbe2a582c
Nadia Kuka PC <nadia.figueroafernandez@epfl.ch>
2016-08-10 18:24:01 +0200

    corrected data visualization

--- tasks/jseds_demos.m
+++ tasks/jseds_demos.m
@@ -5,8 +5,8 @@ clear ros.Bag;
 clear all
 
 %---> MODIFY THESE DIRECTORIES
-bag_dir = '/home/nbfigueroa/Desktop/kinesthetic_recordings/Yo/bags/target_3/';
-data_dir = '/home/nbfigueroa/Desktop/kinesthetic_recordings/Yo/mat/';
+bag_dir = '/home/nbfigueroa/Desktop/kinesthetic_recordings/Yo/bags/pour_no_obst/';
+data_dir = '/home/nbfigueroa/Desktop/kinesthetic_recordings/Yo/mat/pour_no_obst/';
 
 bags = dir(strcat(bag_dir,'*.bag'));
 
@@ -47,7 +47,7 @@ for ii=1:N
 end
 
 %% Save raw data to matfile
-matfile = strcat(data_dir,'raw_data.mat');
+matfile = strcat(data_dir,'data.mat');
 save(matfile,'data','bags','bag_dir')
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -55,32 +55,43 @@ save(matfile,'data','bags','bag_dir')
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Load Data from Mat File
-matfile = strcat(data_dir,'raw_data.mat');
+matfile = strcat(data_dir,'data.mat');
 load(matfile);
 
 %% Visualize Data
 close all;
 
-% Select Bag
-recording = data{1};
-
-% Extract desired variables
-joint_data = recording.kuka.joint_states(1:7,:);
-ee_traj    = recording.kuka.pose(1:3,:);
-
-% Plot Joint Trajectories
-joint_data = joint_states(1:7,:);
 figure('Color',[1 1 1])
-plot(joint_data')
+for i=1:length(data)
+    
+    % Extract desired trajectory
+    recording = data{i};
+    
+    % Extract desired variables
+    joint_states = recording.kuka.joint_states(1:7,:);
+
+    % Plot Joint Trajectories
+    joint_data = joint_states(1:7,:);
+    plot(joint_data');hold on;
+    
+end
 ylabel('rad')
 xlabel('sample')
 title('Joint Trajectories')
 grid on;
 
-% Plot EE Trajectories
-ee_traj = pose(1:3,:);
+
 figure('Color',[1 1 1])
-plot3(ee_traj(1,:), ee_traj(2,:), ee_traj(3,:))
+for i=1:length(data)
+    % Extract desired trajectory
+    recording = data{i};
+        
+    % Extract desired variables
+    ee_traj = recording.kuka.pose(1:3,:);
+    
+    % Plot EE Trajectories
+    plot3(ee_traj(1,:), ee_traj(2,:), ee_traj(3,:)); hold on;
+end
 xlabel('x');ylabel('y');zlabel('z');
 grid on
 title('Cartesian EE Trajectories')
