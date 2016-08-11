9338309ad41700d2de7face8126ab72a9cdc77e7
nadia <nadia.figueroafernandez@epfl.ch>
2016-08-11 14:34:18 +0200

    Fixed some bugs

--- utils/plotEEData.m
+++ utils/plotEEData.m
@@ -1,7 +1,27 @@
-function [ ] = plotEEData( X, t )
+function [ ] = plotEEData( X, t , titlename)
 %UNTITLED8 Summary of this function goes here
 %   Detailed explanation goes here
 
+begin_time = t(1);
+
+figure('Color', [1 1 1]);
+subplot(4,1,1);
+plot(t - begin_time, X(1:3,:)','--');
+legend('x','y','z')
+
+subplot(4,1,2);
+plot(t - begin_time, X(4:7,:)','-');
+legend('q_w','q_i', 'q_j', 'q_k')
+
+subplot(4,1,3);
+plot(t - begin_time, X(8:10,:)','-');
+legend('f_x','f_y', 'f_z')
+
+subplot(4,1,4);
+plot(t - begin_time, X(11:end,:)','-');
+legend('\tau_x','\tau_y', '\tau_z')
+
+suptitle(titlename)
 
 end
 
--- utils/preProcessEEData.m
+++ utils/preProcessEEData.m
@@ -69,25 +69,8 @@ X = [X_p; force; torque];
 t = [ft_t];
 
 % Plot Data from Bag
-if options.do_plot == 1
-    figure('Color', [1 1 1]);
-    subplot(4,1,1);
-    plot(pose_sub_t - begin_time, X(1:3,:)','--');
-    legend('x','y','z')
-
-    subplot(4,1,2);
-    plot(pose_sub_t - begin_time, X(4:7,:)','-');
-    legend('q_w','q_i', 'q_j', 'q_k')
-
-    subplot(4,1,3);
-    plot(ft_t - begin_time, X(8:10,:)','-');
-    legend('f_x','f_y', 'f_z')
-
-    subplot(4,1,4);
-    plot(ft_t - begin_time, X(11:end,:)','-');
-    legend('\tau_x','\tau_y', '\tau_z')
-
-    suptitle(options.title_name)
+if options.do_plot == 1    
+    plotEEData( X, t ,options.title_name)
 end
 
 end
